29b8c83362bcee0e405841f91bd727ac7822aa11
Sovluck45 <88244862+Sovluck45@users.noreply.github.com>
2022-02-23 16:16:18 +0300

    Delete Task_1_6.py

--- Task_1_6.py
+++
@@ -1,7 +0,0 @@
-from chardet import detect
-
-with open('test_file.txt', 'rb') as text_file:
-    content = text_file.read()
-encoded_content = detect(content)
-print(f"Содержимое файла:\n{content.decode('utf-8')}\n")
-print(f"Кодировка файла: {encoded_content}")
